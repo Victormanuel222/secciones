8ad1aba40b7aa91a6815a26ae8f6a1203f346a1c
Victormanuel222 <manuelitovick89@gmail.com>
2023-06-30 11:10:40 -0600

    correciones

--- rutas/usuarios.js
+++ rutas/usuarios.js
@@ -1,17 +1,51 @@
-var rutas=require("express").Router();
-rutas.get("/", (req,res)=>{
-    res.render("inicio");
-});
+var ruta=require("express").Router();
 
-rutas.get("/login",(req,res)=>{
+
+ruta.get("/",(req, res)=>{
+res.render("inicio");
+})
+
+ruta.get("/login",(req,res)=>{
     res.render("login");
-});
+})
 
-rutas.post("/revisar",(req, res)=>{
-    if(req.body.usuarios=="abc" && req.body.password=="123")
-    res.render("Bienvenido");
-    else{
-        res.render("error");
+ruta.post("/revisar",(req,res)=>{
+    console.log(req.body.usuario);
+    console.log((req.body.password));
+    
+    
+    if(req.body.usuario=="abc" && req.body.pass=="123"){
+        req.session.usuario=req.body.usuario
+        res.redirect("/bienvenido");
+    }else{
+        res.redirect("/error");
     }
 });
-module.exports=rutas;
+
+ruta.get("/bienvenido",(req,res)=>{
+    if(req.session.usuario){
+        res.render("bienvenido");
+    }else{
+        res.redirect("/error");
+    }
+
+}) 
+
+ruta.get("/error",(req,res)=>{
+    res.render("error");
+    })
+
+ruta.get("/protegido",(req,res)=>{
+    if(req.session.usuario){
+        res.render("bienvenido");
+    }else{
+        res.redirect("/error");
+    }
+});
+
+ruta.get("/logout", (req,res)=>{
+    req.session.destroy();
+    res.redirect("/")
+})
+
+module.exports=ruta;
